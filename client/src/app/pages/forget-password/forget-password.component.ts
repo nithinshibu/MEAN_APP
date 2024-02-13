386ba552469d38960072bcde370ba1aaa5970210
Nithin Mathew <nithin.mathew@sysfore.com>
2024-02-13 11:43:29 +0530

    Added the forget password initial code

--- client/src/app/pages/forget-password/forget-password.component.ts
+++ client/src/app/pages/forget-password/forget-password.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-forget-password',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './forget-password.component.html',
   styleUrl: './forget-password.component.scss',
 })
-export default class ForgetPasswordComponent {}
+export default class ForgetPasswordComponent implements OnInit {
+  forgetForm!: FormGroup;
+  fb = inject(FormBuilder);
+  ngOnInit(): void {
+    this.forgetForm = this.fb.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+    });
+  }
+  submit() {
+    console.log(this.forgetForm.value);
+  }
+}
